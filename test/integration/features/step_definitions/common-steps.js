f68c5371d002aae617ee41cfb744a6a4a2cd7982
Matt Travi <programmer@travi.org>
2020-08-23 23:32:45 -0500

    test(integrations): passed existing node_modules through the stubbed filesystem

--- test/integration/features/step_definitions/common-steps.js
+++ test/integration/features/step_definitions/common-steps.js
@@ -1,9 +1,13 @@
+import {resolve} from 'path';
 import stubbedFs from 'mock-fs';
 import {After, Before, When} from 'cucumber';
 import {removeGreenkeeper} from '../../../../lib/index.cjs';
 
 Before(async function () {
-  stubbedFs({'README.md': '# some-project'});
+  stubbedFs({
+    node_modules: stubbedFs.load(resolve(__dirname, '../../../../', 'node_modules')),
+    'README.md': '# some-project'
+  });
 });
 
 After(() => stubbedFs.restore());
